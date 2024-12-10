bec2d496d50fc30641e49d72503e17c4d5954fa0
GusTav-DeV22 <gustavo.r05@aluno.ifsc.edu.br>
2024-12-10 20:08:29 -0300

    Novo move perimetro

--- Precoce30s.java
+++ Precoce30s.java
@@ -1,34 +1,51 @@
 package Moves;
 
 import robocode.Robot;
-import robocode.ScannedRobotEvent;
 import robocode.HitRobotEvent;
-import java.awt.Color;
+import robocode.ScannedRobotEvent;
+import java.awt.*;
 
 public class Precoce30s extends Robot {
+    boolean peek; 
+    double moveAmount;
+
     public void run() {
-        // Definir as cores do robô
         setBodyColor(Color.black);
         setGunColor(Color.black);
         setRadarColor(Color.orange);
         setBulletColor(Color.cyan);
         setScanColor(Color.cyan);
+        
+        moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+        peek = false;
 
-        // Movimento básico
-        ahead(100);  // Move o robô para frente 100 unidades
+        turnLeft(getHeading() % 90);
+        ahead(moveAmount);
+        peek = true;
+        turnGunRight(90);
+        turnRight(90);
+
+        while (true) {
+            peek = true;
+            ahead(moveAmount);
+            peek = false;
+            turnRight(90);
+        }
     }
 
-    // Método para lidar com robôs escaneados
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(2);  // Dispara com potência 2 ao detectar um robô
+        fire(2);
+        if (peek) {
+            scan();
+        }
     }
 
-    // Método para lidar com a colisão com outro robô
     public void onHitRobot(HitRobotEvent e) {
         if (e.getBearing() > -90 && e.getBearing() < 90) {
-            back(100);  // Se o robô colidir pela frente, recua 100 unidades
+            back(100);
         } else {
-            ahead(100);  // Caso contrário, avança 100 unidades
+            ahead(100);
         }
     }
 }
+
